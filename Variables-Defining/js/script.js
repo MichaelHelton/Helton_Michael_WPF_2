8a562e6c0b44d5738639ac742d333041106470be
Michael Helton <heltonmike@fullsail.edu>
2014-08-13 17:35:36 -0400

    Defining Variables
    Defining and declaring variables and placing the information inside the
    console.log

---
+++ Variables-Defining/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mikehelton on 8/13/14.
+ */
+
+var age; //declaration
+age = 44; //declaration
+
+//storage container = value
+
+var yearOfBirth = 1969; //declaring and defining on the same line
+
+console.log(age);
+console.log(yearOfBirth);
