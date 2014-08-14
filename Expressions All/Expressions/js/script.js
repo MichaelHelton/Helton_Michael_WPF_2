43e10a9b8aebf31afd91b580541c780b17f9654b
Michael Helton <heltonmike@fullsail.edu>
2014-08-14 10:18:04 -0400

    Arithmetic operators
    using arithmetic operator expressions

---
+++ Expressions All/Expressions/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mikehelton on 8/14/14.
+ */
+
+
+//Expressions - Arithmetic Operators
+
+// area of the triangle is half of the width times the height
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
+
