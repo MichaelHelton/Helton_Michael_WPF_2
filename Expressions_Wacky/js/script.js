c2abff383ea67275b37289293e3e01cd75bc64db
Michael Helton <heltonmike@fullsail.edu>
2014-08-14 19:42:56 -0400

    expressions wacky
    Converting the square footage of a room into how many tile it will take
    to complete the tile project

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,13 +6,13 @@
 //Converting room width and length into square feet and then into how many tile it will take to tile the room
 //prompting user to input room width
 var roomWidth = prompt("What is the width of your room?");
-console.log=(roomWidth);
-document.write=(roomWidth);
+console.log(roomWidth);
+
 
 //prompting user to input room length
 var roomLength = prompt("What is the length of your room?");
 console.log(roomLength);
-document.write(roomLength);
+
 
 //answer of how many square feet you have
 answer = (roomWidth * roomLength+ " square feet");
@@ -23,6 +23,11 @@ alert ("Tile sizes to choose from are 2, 4, 6, 8, 10, 12, 18 & 24");
 
 //converting to tile size being used to tile room
 var tileSize = [2, 4, 6, 8, 10, 12, 18, 24];
+tileSize = prompt("What is the size of your tile?");
+answer = (roomWidth * roomLength * 12 / tileSize);
+console.log(answer+ " tile needed to complete this project");
+document.write(answer+ " tile needed to complete this project");
+alert (answer+ " tile needed to complete this project");
 
 
 
