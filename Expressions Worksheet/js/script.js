7ae971a9cee3905d9c11c6a84745511ab9aa4b6a
Michael Helton <heltonmike@fullsail.edu>
2014-08-14 12:00:20 -0400

    dog years
    1 human year equals 7 dog years

--- Expressions Worksheet/js/script.js
+++ Expressions Worksheet/js/script.js
@@ -5,4 +5,5 @@
 //Dog Years
 
 var age = 7; //Sparky's age now
-
+age = age * 7; //Sparky's age in dog years (1 human year is 7 dog years)
+console.log(age); //Sparky's age should be 7 human years X 7 dog years = 49
