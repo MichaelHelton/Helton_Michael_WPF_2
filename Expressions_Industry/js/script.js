86572559b7f480eb421ec1b8029b3e3ddf2b3c8c
Michael Helton <heltonmike@fullsail.edu>
2014-08-14 17:01:21 -0400

    Assignment:Industry
    prompting for the pip of the image

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,3 +6,7 @@
 var dPerInch = prompt("What dpi is your image?");
 console.log(dPerInch);
 
+//prompting for the ppi
+var pPerInch = prompt("What are the ppi of the image?");
+console.log(pPerInch);
+
