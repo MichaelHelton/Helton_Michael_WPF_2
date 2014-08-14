79f6d7481ccb9a71d082445ca96b8e893a0dc59b
Michael Helton <heltonmike@fullsail.edu>
2014-08-14 11:18:31 -0400

    Expressions with Arrays
    Expressions using arrays

---
+++ Expressions All/Expressions - With Arrays/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mikehelton on 8/14/14.
+ */
+
+//Expressions - Expressions with Arrays
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
