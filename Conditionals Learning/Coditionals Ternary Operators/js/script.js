c59564da01831147ef3c72fdce3d59b4cdc28ee8
Michael Helton <heltonmike@fullsail.edu>
2014-08-19 12:21:38 -0400

    Conditionals
    Going through the Conditionals videos following along

---
+++ Conditionals Learning/Coditionals Ternary Operators/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mikehelton on 8/19/14.
+ */
+//Conditional Logic - Ternary Operators
+
+var age = 6;
+var book;
+
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time machine
+book = (age<10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
---
+++ Conditionals Learning/Conditionals Logic/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by mikehelton on 8/19/14.
+ */
+//Conditional Logic - else if
+
+var kidHeight = 52;
+var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
+
+//if child is old enough, print to the console "you can ride!"
+//if kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //you can ride
+    console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do
+    console.log("Sorry kid, you've got some growing to do first!");
+}
