fde80e412a6135cad571867b85739f88a8ad94ac
Michael Helton <heltonmike@fullsail.edu>
2014-08-20 08:45:07 -0400

    celsius converter
    Converting Celsius into Fahrenhiet

---
+++ Conditionals Worksheet/Celsius to Fahrenheit converter/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by mikehelton on 8/20/14.
+ */
+//Celsuis to Fahrenheit converter/
+
+//prompt user to enter temperature to convert
+var C = prompt ("What temperature would you like to convert to Fahrenheit?");
+answer = (C * 9 / 5 + 32 + " Degrees Fahrenheit");
