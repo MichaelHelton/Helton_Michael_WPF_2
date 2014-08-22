621175063d4bfe541cb6b68977fa7c2d6d3f05e3
Michael Helton <heltonmike@fullsail.edu>
2014-08-21 20:30:57 -0400

    3rd else if
    writing 3rd else if to the console

--- Conditionals Worksheet/Conditionals Grade/js/script.js
+++ Conditionals Worksheet/Conditionals Grade/js/script.js
@@ -17,4 +17,8 @@ if (grade >= 100){
 //else if grade is this
 }else if (grade >= 80){
     console.log(grade + "%" + " You have earned an B in this class!");
+//else if grade is this
+}else if (grade >=70){
+    //writing to console
+    console.log(grade + "%" + " You have earned an C in this class!");
 }
