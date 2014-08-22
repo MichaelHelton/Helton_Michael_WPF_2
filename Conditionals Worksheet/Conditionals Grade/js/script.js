6b98e212e97a805392563e72abf3b4a2275ef5da
Michael Helton <heltonmike@fullsail.edu>
2014-08-21 20:24:37 -0400

    if statement
    if statement and rioting to the console

--- Conditionals Worksheet/Conditionals Grade/js/script.js
+++ Conditionals Worksheet/Conditionals Grade/js/script.js
@@ -6,3 +6,8 @@
 
 //asking for grade
 var grade = parseFloat(prompt("What is your number grade?"));
+//if grade is this
+if (grade >= 100){
+    //writing to the console
+    console.log(grade + "%" + " You have earned an A+ in this class!");
+}
