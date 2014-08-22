0200ab98d1324ccb92553f1eefe71015b2dd1b5a
Michael Helton <heltonmike@fullsail.edu>
2014-08-21 20:54:25 -0400

    prompting user for age
    prompting user for age

---
+++ Conditionals Worksheet/Conditionals Movie Ticket Price/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mikehelton on 8/21/14.
+ */
+//Movie Ticket price
+
+//prompting for age
+var age = prompt ("What is your age?");
+
+//if statement
+if (age >= 54){
+//else if
+}
