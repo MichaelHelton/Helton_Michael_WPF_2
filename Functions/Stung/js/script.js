a72e7e649f3eee6e3555e588372e950f912add70
Michael Helton <heltonmike@fullsail.edu>
2014-08-28 09:53:55 -0400

    prompting user
    prompting user for the weight of their animal

--- Functions/Stung/js/script.js
+++ Functions/Stung/js/script.js
@@ -6,3 +6,7 @@
 //writing to the console and alerting user of how mmany bee stings it takes to kill an animal
 console.log("It takes 8.666666667 bee stings per pound to kill an animal.");
 alert("It takes 8.666666667 bee stings per pound to kill an animal.");
+
+//prompting user for weight of their animal
+var weight = prompt("How much does your animal weigh?");
+
