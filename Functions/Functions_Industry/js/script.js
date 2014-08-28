653aa6c4fd5c07326ec7da11f3103b7eb5d90f63
Michael Helton <heltonmike@fullsail.edu>
2014-08-28 18:41:54 -0400

    prompting user
    prompting user for information

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -1,3 +1,9 @@
 /**
- * Created by mikehelton on 8/20/14.
+ * Michael Helton
+ * WPF Section 1
+ * Function Industry
  */
+
+//prompting user with how many changes will be made to website
+var changes = prompt("How many changes will you have this month?");
+console.log(changes);
