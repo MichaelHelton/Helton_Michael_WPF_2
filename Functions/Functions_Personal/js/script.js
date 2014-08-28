f9747dcd315c3d326be2efac905e6103bea8560c
Michael Helton <heltonmike@fullsail.edu>
2014-08-28 17:16:07 -0400

    prompting user again
    prompting user for the length of the room

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -8,3 +8,10 @@
 var width = prompt("What is the width of your room?");
 //writing to console
 console.log(width);
+
+//function
+function calcRoom(){
+    //prompting user for the length of room
+    var length = prompt("What is the length of your room?");
+    
+}
