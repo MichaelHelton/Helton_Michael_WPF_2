72a5c1f18d8e243c9bbbd56469fde82e5711cfc3
Michael Helton <heltonmike@fullsail.edu>
2014-08-28 18:50:38 -0400

    writing to the console
    writing to the console

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -19,3 +19,6 @@ if(changes <= 10){
 var calcHours = prompt("Calculate what the cost will be by entering how many hours you think it will take.");
 //totaling up what the user thinks and the amount per hour plus the normal monthly fee
 total = (calcHours * 35 + 40);
+//writing to the console
+console.log("$" + total + " for this month.");
+console.log("Thank you for your buiness!");
