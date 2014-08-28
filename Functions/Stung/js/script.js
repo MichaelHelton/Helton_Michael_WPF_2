5e7c5f65ff5a07b03ff567b0e6789d7c087e8d97
Michael Helton <heltonmike@fullsail.edu>
2014-08-28 09:52:15 -0400

    writing to console and alerting
    writing to console and alerting user to the bites it takes to kill an
    animal

---
+++ Functions/Stung/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by mikehelton on 8/28/14.
+ Stung
+ */
+
+//writing to the console and alerting user of how mmany bee stings it takes to kill an animal
+console.log("It takes 8.666666667 bee stings per pound to kill an animal.");
+alert("It takes 8.666666667 bee stings per pound to kill an animal.");
